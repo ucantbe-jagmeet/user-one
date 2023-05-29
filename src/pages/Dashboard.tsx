068fb31b5703d7cea44f9a602eb98c09e95df8ab
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-29 17:59:42 +0530

    search component created

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
 
 const Dashboard: FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <main className="h-[100vh]  flex bg-slate-200 ">
+        <div className="w-[100vw] flex justify-around">
+          <h1>Search</h1>
+          <h1>Create one</h1>
+        </div>
+
+        {/* <div className="h-auto w-[80%]  bg-slate-200 rounded-md  md:w-[55%] lg:w-[30%] py-4 px-6 shadow-lg"></div> */}
+      </main>
+    </>
+  );
 };
 
 export default Dashboard;
