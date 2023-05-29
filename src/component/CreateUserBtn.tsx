03bc8fcfe5bb0437e8c19a0a4c577a295ee6ff9b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-29 18:21:30 +0530

    Search component completed

---
+++ src/component/CreateUserBtn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateUserBtn = () => {
+  return <div>createUserBtn</div>;
+};
+
+export default CreateUserBtn;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
+import Search from "../component/Search";
+import CreateUser from "../component/CreateUser";
 
 const Dashboard: FC = () => {
   return (
     <>
       <main className="h-[100vh]  flex bg-slate-200 ">
-        <div className="w-[100vw] flex justify-around">
-          <h1>Search</h1>
-          <h1>Create one</h1>
+        <div className="w-[100vw] flex-col items-center flex md:flex-row md:justify-around md:items-start p-10">
+          <Search />
+          <CreateUser />
         </div>
 
         {/* <div className="h-auto w-[80%]  bg-slate-200 rounded-md  md:w-[55%] lg:w-[30%] py-4 px-6 shadow-lg"></div> */}
